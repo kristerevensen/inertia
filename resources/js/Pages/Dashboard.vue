882b1b91d1408d397d0176a4dac43e39985defb8
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2023-12-30 23:44:57 +0100

    30 dec 2023

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -20,25 +20,28 @@ function submit() {
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <div class="flex justify-between">
+                <div>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Projects
             </h2>
+            </div>
+            <div>
+                <button type="button"
+                                class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                Add Project</button>
+            </div>
+            </div>
+
+
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px lg:px-0">
                 <div class=" sm:px-6 lg:px-8">
-                    <div class="sm:flex sm:items-center">
-                        <div class="sm:flex-auto">
-                            <h1 class="text-base font-semibold leading-6 text-gray-900">Projects</h1>
-                            <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their
-                                name, title, email and role.</p>
-                        </div>
-                        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                            <button type="button"
-                                class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                Add Project</button>
-                        </div>
+                    <div class="">
+
+
                     </div>
                     <div class="mt-8 flow-root">
                         <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
