901bbe139b5d5c010fa8deffcf814c4263fa97a2
kristerevensen <67630525+kristerevensen@users.noreply.github.com>
2024-01-11 13:07:42 +0100

    removed migratino file

--- database/migrations/2024_01_11_120710_create_filters_table.php
+++ database/migrations/2024_01_11_120710_create_filters_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('data_pages_filters', function (Blueprint $table) {
+        Schema::create('filters', function (Blueprint $table) {
             $table->id();
             $table->string('project_code'); //project code
             $table->text('filters'); //serialized array of url parameters to be removed from the urls
-            $table->foreign('project_code_ref')->references('project_code')->on('projects'); // foreign key constraint
+            $table->foreign('project_code')->references('project_code')->on('projects'); // foreign key constraint
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('data_pages_filters');
+        Schema::dropIfExists('filters');
     }
 };
